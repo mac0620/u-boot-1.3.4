12700fc01bae2c895ef21708574e67ba047d4cb7
mac <mac0620@126.com>
2021-02-26 16:08:01 +0800

    fix original build error.

--- lib_arm/board.c
+++ lib_arm/board.c
@@ -134,19 +134,19 @@ char *strmhz(char *buf, long hz)
  ************************************************************************
  * May be supplied by boards if desired
  */
-void inline __coloured_LED_init (void) {}
+void  __coloured_LED_init (void) {}
 void inline coloured_LED_init (void) __attribute__((weak, alias("__coloured_LED_init")));
-void inline __red_LED_on (void) {}
+void  __red_LED_on (void) {}
 void inline red_LED_on (void) __attribute__((weak, alias("__red_LED_on")));
-void inline __red_LED_off(void) {}
+void  __red_LED_off(void) {}
 void inline red_LED_off(void)	     __attribute__((weak, alias("__red_LED_off")));
-void inline __green_LED_on(void) {}
+void  __green_LED_on(void) {}
 void inline green_LED_on(void) __attribute__((weak, alias("__green_LED_on")));
-void inline __green_LED_off(void) {}
+void  __green_LED_off(void) {}
 void inline green_LED_off(void)__attribute__((weak, alias("__green_LED_off")));
-void inline __yellow_LED_on(void) {}
+void  __yellow_LED_on(void) {}
 void inline yellow_LED_on(void)__attribute__((weak, alias("__yellow_LED_on")));
-void inline __yellow_LED_off(void) {}
+void  __yellow_LED_off(void) {}
 void inline yellow_LED_off(void)__attribute__((weak, alias("__yellow_LED_off")));
 
 /************************************************************************
